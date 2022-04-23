28d34a5b9550fe2714588710f13f3b796f9482b8
Adrian Ajvazaj <at059219@adrians-mbp.lan>
2022-04-23 16:48:08 +0200

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+    return (
+        <div>
+            <h1>Hi there from Component React App</h1>
+            <MarketingApp />
+        </div>
+    )
+}
